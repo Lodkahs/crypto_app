79a0eed2b05052057e550b785e635b08e96bb582
Lodkahs <omalojiu@gmail.com>
2023-03-19 09:50:34 +0200

    model

---
+++ CryptoApp/Model/CryptoCurrency.swift
@@ -0,0 +1,15 @@
+//
+//  CryptoCurrency.swift
+//  CryptoApp
+//
+//  Created by Andrew  on 3/19/23.
+//
+
+import Foundation
+
+struct CryptoCurrency : Codable {
+    
+    var currency : String
+    var price : String
+    
+}
