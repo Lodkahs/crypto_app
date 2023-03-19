5ddac29a6b0ace2f6d370586a7f167565090a0f5
Lodkahs <omalojiu@gmail.com>
2023-03-19 10:38:39 +0200

    web service, url setup

---
+++ CryptoApp/Service/WebService.swift
@@ -0,0 +1,33 @@
+//
+//  WebService.swift
+//  CryptoApp
+//
+//  Created by Andrew  on 3/19/23.
+//
+
+import Foundation
+
+class WebService {
+    
+    func downloadCurrencies(url : URL, completion: @escaping ([CryptoCurrency]?) -> ()) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                
+                let cryptoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                 
+                if let cryptoList = cryptoList {
+                    completion(cryptoList)
+                }
+                
+            }
+            
+        }.resume()
+        
+    }
+    
+}
